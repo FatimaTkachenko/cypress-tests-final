893230f4973fffc205376e94d60c4fcca5e4b29c
Fatima <fat.tka4enko2014@yandex.ru>
2026-05-17 14:13:12 +0300

    Все 5 тестов успешно проходят

--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -20,4 +20,12 @@ describe('Тесты приложения Книги', () => {
   it('Страница содержит элементы', () => {
     cy.get('body').should('not.be.empty')
   })
+
+  it('На странице есть хотя бы одна кнопка', () => {
+    cy.get('button').should('have.length.at.least', 1)
+  })
+
+  it('Заголовок страницы не пустой', () => {
+    cy.title().should('not.be.empty')
+  })
 })
